81470906bcddcd71d05f79fd2b1b5fe47f1ff521
Kyle Kravette <kkravette@gmail.com>
2014-01-19 20:41:34 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HhsTheatre::Application.config.secret_key_base = 'f42a3427cf2c2f3d9c969bb1b4816be850b14fa3ab2c99c685eb0c4323eda83a9dbb06dca6f935985110a2dc66e048e33cc430f1444f03d3537f9dd1caaafe95'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HhsTheatre::Application.config.session_store :cookie_store, key: '_hhs_theatre_session'
